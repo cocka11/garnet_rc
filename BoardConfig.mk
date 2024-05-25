0a8755e67aee6440e30905f2113c5616f64aa412
Darth9 <reyprimerjefe@gmail.com>
2024-05-25 10:24:52 +0100

    Add some board settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,4 +51,11 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # device-specific fstab
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
+# -------- from lineage DT -------------
+# we're qcom
+BOARD_USES_QCOM_HARDWARE := true
+
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
 #
