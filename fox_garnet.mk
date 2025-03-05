ec5d6c8810484940e7d0327e3f59b869671f3011
Darth9 <reyprimerjefe@gmail.com>
2025-03-05 09:18:51 +0000

    Remove duplicate entry

--- fox_garnet.mk
+++ fox_garnet.mk
@@ -48,9 +48,6 @@ OF_WIPE_METADATA_AFTER_DATAFORMAT := 1
 # avoid MTP issues after data format
 OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
 
-# we have a recovery partition
-OF_AB_DEVICE_WITH_RECOVERY_PARTITION := 1
-
 # don't spam the console with loop errors
 OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
 
